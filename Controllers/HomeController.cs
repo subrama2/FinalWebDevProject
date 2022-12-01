b60aed1137d37c65d44c244355741cf4ca99eb48
Annamalai Subramanian <subrama2@mail.uc.edu>
2022-12-01 17:40:35 -0500

    added index method thus beginner about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,21 +13,23 @@ namespace FinalWebDevProject.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
-        {
-            return View("About");
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult About()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+        public IActionResult Food()
+        {
+            return View();
+        }
+
+
+
+
+
     }
 }
